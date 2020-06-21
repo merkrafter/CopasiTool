ec999216d6e1d47c8a62fe145fd46588f08a96a8
merkrafter <merkrafter@gmail.com>
2020-06-21 18:02:28 +0200

    Create functional reactions function for subtraction

--- models/CopasiTool.py
+++ models/CopasiTool.py
@@ -32,7 +32,6 @@ class CopasiModel:
         self.reactions = []
         # A functional reaction is a set of reactions that are tuned to represent a single higher order function as addition or division.
         self.num_functional_reactions = 0
-        self.num_intermediate_species = 0
         # This null species is expected to exist by later functions.
         # It serves as a "bin", that is, other species that should decay in reality are converted to null in COPASI
         self.null = CopasiSpecies("null")
@@ -70,6 +69,25 @@ class CopasiModel:
         self.reactions += [R1, R2, R3]
         #self.species.append(Y)
 
+    def create_SUB_reactions(self, X1, X2, Y):
+        """
+        Returns reactions that represent the function: Y = X1 - X2 if X1 >= X2
+        Their naming scheme includes the type of function, an id, and descriptions of each reaction
+        using the literals "X1", "X2", and "Y" instead of the species these variables stand for.
+        This way it is easier to recognize their purpose in Copasi. For instance, if it just said
+        "x25 -> x25 + intermediate2342" nobody would know what the purpose of that x25 is.
+        """
+        name_prefix = "Sub{}_".format(self.num_functional_reactions)
+        self.num_functional_reactions += 1
+        
+        Z = CopasiSpecies(name_prefix+"Z")
+        self.add_species(Z)
+        
+        R1 = CopasiReaction(name_prefix+"X1toY", substrates=[(1,X1)], products=[(1,X1), (1,Y)])
+        R2 = CopasiReaction(name_prefix+"X2toZ", substrates=[(1,X2)], products=[(1,X2), (1,Z)])
+        R3 = CopasiReaction("YandZdecay", substrates=[(1,Y), (1,Z)], products=[(1,self.null)])
+        R4 = CopasiReaction("Ydecay", substrates=[(1, Y)], products=[(1,self.null)])
+        self.reactions += [R1, R2, R3, R4]
 
 def create_copasi_file_from_template(template_path, species, reactions):
     env = Environment(loader=FileSystemLoader(searchpath="./"), autoescape=True)
@@ -84,7 +102,7 @@ if __name__ == "__main__":
     X2 = model.add_species(name="X2", initial_concentration=1)
     Y  = model.add_species(name="Y")
     
-    model.create_ADD_reactions(X1, X2, Y)
+    model.create_SUB_reactions(X1, X2, Y)
     
     with open("result.cps", "wb") as f:
         f.write(create_copasi_file_from_template("template.cps.jinja", model.species_list, model.reactions).encode("utf-8"))
