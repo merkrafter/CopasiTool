a75c6191374f73b4ff8cb4447fadade7395c3476
Mark Umnus <merkrafter@gmail.com>
2020-06-26 17:01:41 +0200

    Extract util from CopasiTool
    This makes the util functions easier to use everywhere else

--- CopasiTool.py
+++ CopasiTool.py
@@ -1,22 +1,5 @@
 from copasi_model import yaml2model
-
-import logging
-
-
-def setup_logger(args):
-    logger = logging.getLogger(__name__)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter('[%(levelname)s] %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    if args.verbose >= 2:
-        logger.setLevel(logging.DEBUG)
-    elif args.verbose == 1:
-        logger.setLevel(logging.INFO)
-    else:
-        logger.setLevel(logging.WARNING)
-    return logger
-
+from util import setup_logger
 
 if __name__ == "__main__":
     import argparse
